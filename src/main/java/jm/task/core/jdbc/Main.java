c8d2ffb2d0cb06ef9f6ee3491a90b539c2abb9a7
HeyUgly <heyuglysnail@gmail.com>
2024-05-28 15:57:06 +0300

    PP_1_1_3-4_JDBC_Corrected
    -Поправил пакетный импорт
    
    -Убрал удаление таблицы через TRUNCATE
    
    -Вынес Connection в поле класса
    
    -Сделал объявления через интерфейс, чтобы работал полиморфизм
    
    -Спрятал поля в private final
    
    -Излишний toString потёр

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
 
-        userService.createUsersTable();
-        userService.saveUser("John", "Jameson", (byte) 83);
-        userService.saveUser("Paul", "McCartney", (byte) 81);
-        userService.saveUser("Maisie", "Williams", (byte) 27);
-        userService.saveUser("Bill", "Gates", (byte) 68);
-        System.out.println(userService.getAllUsers().toString());
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        try {
+            userService.createUsersTable();
+            userService.saveUser("John", "Jameson", (byte) 83);
+            userService.saveUser("Paul", "McCartney", (byte) 81);
+            userService.saveUser("Maisie", "Williams", (byte) 27);
+            userService.saveUser("Bill", "Gates", (byte) 68);
+            System.out.println(userService.getAllUsers());
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
